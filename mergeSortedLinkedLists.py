ae1adcb8c945441a03ed49560eac9fe0118b67cd
Mathieu Charbonnel <mathieu.charbonnel@gmail.com>
2024-01-06 20:19:16 +0100

    first hard problem from leetcode

---
+++ mergeSortedLinkedLists.py
@@ -0,0 +1,48 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def insert(self, lists, element):
+        index = 0
+        while index < len(lists) and element.val > lists[index].val:
+            index += 1
+
+        lists.insert(index, element)
+
+    def list_update(self, lists):
+        lists[0] = lists[0].next
+        if not lists[0]:
+            lists.pop(0)
+        else:
+            el = lists[0]
+            lists.pop(0)
+            self.insert(lists, el)
+
+    def rev(self, linked_list):
+        if not linked_list:
+            return None
+        rev_list = ListNode(linked_list.val)
+        linked_list = linked_list.next
+        while linked_list:
+            rev_list = ListNode(linked_list.val, rev_list)
+            linked_list = linked_list.next
+        return rev_list
+
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        lists = [el for el in lists if el]
+        lists = sorted(lists, key=lambda x: x.val)
+
+        # Initialization
+        if not lists:
+            return None
+        merged = ListNode(lists[0].val)
+        self.list_update(lists)
+
+        while lists:
+            merged = ListNode(lists[0].val, next=merged)
+            self.list_update(lists)
+
+        return self.rev(merged)
